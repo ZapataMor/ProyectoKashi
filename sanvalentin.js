87d88ce1e64fdb7f91a008511ef8fe76694b7f8e
ZapataMor <luiszapataa2017@gmail.com>
2025-02-07 14:12:36 -0500

    Primer commit

---
+++ sanvalentin.js
@@ -0,0 +1,17 @@
+const botonSi = document.getElementById('si');
+    const botonNo = document.getElementById('no');
+
+    botonSi.addEventListener('click', () => {
+      window.location.href = 'respuesta_si.html';
+    });
+
+    botonNo.addEventListener('click', () => {
+      // Aumentar el tamaño del botón "Sí"
+      const fontSize = parseInt(window.getComputedStyle(botonSi).fontSize);
+      botonSi.style.fontSize = `${fontSize + 5}px`;
+
+      // Mover el botón "No" aleatoriamente
+      const randomX = Math.random() * 200 - 100;
+      const randomY = Math.random() * 200 - 100;
+      botonNo.style.transform = `translate(${randomX}px, ${randomY}px)`; 
+    });
